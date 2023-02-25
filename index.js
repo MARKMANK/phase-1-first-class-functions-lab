94c69c68db4b3bd2c4f18af9f405f75b671a7ece
kevinmarkman <kevin.w.markman@gmail.com>
2023-02-25 13:12:07 -0600

    add functions for selecting drivers

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(arrayOfDrivers){
+    return [arrayOfDrivers[0],arrayOfDrivers[1]];
+};
+
+const returnLastTwoDrivers = function(arrayOfDrivers){
+    return arrayOfDrivers.slice(arrayOfDrivers.length-2)
+};
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+ 
+
+function createFareMultiplier(integer){
+    return function(fare){
+      return fare * integer;
+    }
+};
+
+const fareDoubler = function(fare) {
+    const funtionMultiplier = createFareMultiplier(2);
+    return funtionMultiplier(fare)
+};
+
+const fareTripler = function(fare){
+    return createFareMultiplier(fare)(3)
+};
+
+function selectDifferentDrivers (arrayOfDrivers,whichDrivers){
+    return whichDrivers(arrayOfDrivers);
+}
